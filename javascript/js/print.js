0a0670e3052bc9e890a1489041d5d5d95827bc33
silverchill <ustinsteve@gmail.com>
2024-11-21 23:47:30 +0100

    printed my string

---
+++ javascript/js/print.js
@@ -0,0 +1,19 @@
+const myVar = "JavaScript is amazing";
+
+console.log(myVar);
+
+
+
+const myNewVar = "JavaScript is amazing";
+
+console.log("NEXTGEN's " + myNewVar);
+
+
+
+const newName = "Augustine";
+const age = 65;
+const JavaScript = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("likes JavaScript:", JavaScript);
